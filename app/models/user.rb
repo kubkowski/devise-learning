04842c3f1f0727e5d003826d6f2750943e56c701
kubkowski <jakub.opara@gmail.com>
2014-07-18 10:33:27 +0200

    Added email or username registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :signin
+  validates :username, :uniqueness => {:case_sensitive => false}
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+		conditions = warden_conditions.dup
+		where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => @signin.downcase }]).first
+	end
 end
