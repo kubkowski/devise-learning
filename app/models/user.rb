a89ee422e44b4d5f91ac15e64086ef92e3114277
kubkowski <jakub.opara@gmail.com>
2014-07-19 10:25:08 +0200

    Some minor fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :signin
   validates :username, :uniqueness => {:case_sensitive => false}
 
-  def self.find_first_by_auth_conditions(warden_conditions)
-		conditions = warden_conditions.dup
-		where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => @signin.downcase }]).first
-	end
 end
